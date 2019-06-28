2209b2a85cb5e94c30c4578533d8c769a7e0f0cb
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-06-29 02:25:14 +1200

    Wrote dummy api using gin

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module github.com/jordansimsmith/are-you-hitler/api
 
 go 1.12
+
+require github.com/gin-gonic/gin v1.4.0
--- api/main.go
+++ api/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("hello world!")
+	fmt.Println("Are You Hitler? API starting.")
+
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world!",
+		})
+	})
+
+	r.Run(":3000")
 }
