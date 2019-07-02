89590e1a7efbf1eadf32b0c3d1bf3639894f9853
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-07-02 23:26:33 +1200

    Added validation methods for player and game structs

--- api/models/models.go
+++ api/models/models.go
@@ -13,12 +13,12 @@ type User struct {
 }
 
 /*
-Function Validate checks the integrity of each external (json) field
+Method Validate checks the integrity of each external (json) field
 and returns an error if invalid. It is used to sanitise user input.
 */
 func (u User) Validate() error {
 	if u.ID != 0 {
-		return errors.New("cannot provide an ID")
+		return errors.New("ID is generated, not supplied")
 	}
 	if u.FirstName == "" {
 		return errors.New("first_name cannot be blank")
@@ -45,13 +45,51 @@ type Player struct {
 }
 
 /*
+Method Player checks the integrity of each external (json) field
+supplied by the client. It returns an error if invalid.
+*/
+func (p Player) Validate() error {
+	if p.Faction != "fascist" && p.Faction != "liberal" {
+		return errors.New("faction must be one of 'fascist' or 'liberal'")
+	}
+	if p.UserID == 0 {
+		return errors.New("user_id cannot be blank")
+	}
+	if p.Hitler && p.Faction != "fascist" {
+		return errors.New("hitler must be a fascist")
+	}
+	return nil
+}
+
+/*
 Struct Game represents a record of a game between a group of Users.
 The winner of the game is either 'liberal' or 'fascist'.
 */
 type Game struct {
 	ID         uint     `json:"game_id"`
-	Timestamp  int64    `json:"time_stamp"`
+	Timestamp  uint64   `json:"timestamp"`
 	Winner     string   `json:"winner"`
 	NumPlayers uint     `json:"number_of_players"`
 	Players    []Player `json:"players"`
+}
+
+/*
+Method Validate checks the integrity of each external (json) field
+and returns an error if invalid. It only checks the fields supplied
+by the client.
+*/
+func (g Game) Validate() error {
+	if g.ID != 0 {
+		return errors.New("ID is generated, not supplied")
+	}
+	if g.Timestamp == 0 {
+		return errors.New("timestamp not provided")
+	}
+	if g.Winner != "facist" && g.Winner != "liberal" {
+		return errors.New("winner must be one of 'fascist' or 'liberal'")
+	}
+	if g.NumPlayers != uint(len(g.Players)) {
+		return errors.New("inconsistent number of players")
+	}
+	return nil
 }
