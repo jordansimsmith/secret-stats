d902350df2bbd8b9606436f5bd7d3066b62f6a11
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-07-04 04:47:32 +1200

    Implemented user fetching functionality for user page

--- client/src/pages/users.js
+++ client/src/pages/users.js
@@ -1,11 +1,69 @@
 import React from 'react';
+import {
+  Container,
+  Header,
+  Icon,
+  Message,
+  Card,
+  Divider,
+} from 'semantic-ui-react';
+import axios from 'axios';
+
+const API = 'http://localhost:3000';
 
 export class Users extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+    };
+  }
+
   render() {
+    const {users, error} = this.state;
+
+    // check for error when fetching user data
+    if (error) {
+      return (
+        <Message color="red">
+          <Message.Header>
+            An error was encountered when fetching user data.
+          </Message.Header>
+          <p>{error.message}</p>
+        </Message>
+      );
+    }
+
+    // success
     return (
-      <div>
-        <h1>Users</h1>
-      </div>
+      <Container>
+        <Header as="h1" icon textAlign="center">
+          <Icon name="user" />
+          <Header.Content>Users</Header.Content>
+        </Header>
+
+        <Divider />
+
+        <Card.Group centered>
+          {users.map(user => (
+            <Card key={user.user_id}>
+              <Card.Content>
+                <Card.Header>
+                  {user.first_name} {user.last_name}
+                </Card.Header>
+              </Card.Content>
+            </Card>
+          ))}
+        </Card.Group>
+      </Container>
     );
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${API}/users`)
+      .then(res => this.setState({users: res.data}))
+      .catch(error => this.setState({error}));
   }
 }
