f758bee2d1fc91f0a9092f098fee1657ebf065d9
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-07-04 00:29:18 +1200

    Added semantic ui react and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Header} from 'semantic-ui-react';
 
 export class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, World</h1>
+        <Header>Hello, World!</Header>
       </div>
     );
   }
