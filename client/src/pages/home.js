bd46e85904fc322d0afb341948d776ecfea0b668
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-07-10 18:53:09 +1200

    Added get started button to home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {Container, Header, Button} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 
 export class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Container>
+        <Header as="h1" textAlign="center">
+          <Header.Content>Are You Hitler?</Header.Content>
+        </Header>
+
+        <Button as={Link} to="/about">
+          Get Started
+        </Button>
+      </Container>
     );
   }
 }
