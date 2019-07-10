2d8c3d621ad69b1a753680c8101c6bebfb3b007c
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-07-10 19:37:34 +1200

    Added games by user to game page

--- client/src/pages/games.js
+++ client/src/pages/games.js
@@ -6,33 +6,52 @@ import {
   Message,
   Divider,
   Card,
+  Button,
 } from 'semantic-ui-react';
 import axios from 'axios';
+import qs from 'query-string';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 import {CreateGame} from '../components/CreateGame';
 import {GameCard} from '../components/GameCard';
 
 const API = 'http://localhost:3000';
 
 export class Games extends React.Component {
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+  };
+
   constructor(props) {
     super(props);
 
     this.state = {
       games: [],
-      error: null,
     };
 
     this.getGames = this.getGames.bind(this);
   }
 
   render() {
-    const {games, error} = this.state;
+    const {games, error, user} = this.state;
+
+    const allGames = () => {
+      this.setState({user: undefined});
+      this.getGames();
+    };
 
     return (
       <Container>
         <Header as="h1" icon textAlign="center">
           <Icon name="user secret" />
-          <Header.Content>Games</Header.Content>
+          <Header.Content>
+            {user ? `${user.first_name} ${user.last_name}'s games` : 'Games'}
+          </Header.Content>
+          {user && (
+            <Button as={Link} to="/games" onClick={allGames}>
+              Show All Games
+            </Button>
+          )}
         </Header>
 
         <Divider />
@@ -58,12 +77,28 @@ export class Games extends React.Component {
   }
 
   componentDidMount() {
-    this.getGames();
+    // extract user_id from query
+    const userID = qs.parse(this.props.location.search, {
+      ignoreQueryPrefix: true,
+    }).user_id;
+
+    userID && this.getUser(userID);
+    userID ? this.getGames(userID) : this.getGames();
   }
 
-  getGames() {
+  getUser(userID) {
     axios
-      .get(`${API}/games`)
+      .get(`${API}/users/${userID}`)
+      .then(res => this.setState({user: res.data}))
+      .catch(error => this.setState({error}));
+  }
+
+  getGames(userID) {
+    // structure request url
+    const url = userID ? `${API}/games?user_id=${userID}` : `${API}/games`;
+
+    axios
+      .get(url)
       .then(res => this.setState({games: res.data.reverse()}))
       .catch(error => this.setState({error}));
   }
