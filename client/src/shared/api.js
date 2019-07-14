f50ae7afdebe5f3de7f1327352b1ac62bb190c9b
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-07-14 18:06:04 +1200

    Configured nginx reverse proxy for api

--- client/src/shared/api.js
+++ client/src/shared/api.js
@@ -1,4 +1,6 @@
+const PROD_HOST = 'localhost';
+
 export const API =
   process.env.NODE_ENV === 'production'
-    ? 'http://production-url.com'
+    ? `http://${PROD_HOST}/api`
     : 'http://localhost:3000';
